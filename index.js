f3f753bcb0ab761b2f6d5a9241ba766e6ae17baf
marvinwilliam <la805779602@hotmail.com>
2016-10-09 20:18:23 +0800

    取消内部创建日志记录器,由外部提供log4js对象,如果没有,默认使用console输出日志.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var Config   = require('./lib/config/index'),
-    Registry = require('./lib/registery/index');
+    Registry = require('./lib/registery/index'),
+    Log      = require('./lib/log/index');
 
 module.exports = {
-    init: function (options) {
+    init: function (options, logger) {
         Config.load(options);
+        //初始化呢日志记录器,由外部传入log4js对象
+        Log.init(logger);
         Registry.init();
     },
     registry: function (services) {
